6c3e889697878578f52237accfc65184064a8f43
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-07-30 15:36:02 -0400

    runner - for simple and promise

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
 'use strict';
 
-var log=console;
+var log = console;
 
 log.info('Queue.push(hello)');
 
 var maxCount = 1e6;
+var expected = maxCount * (maxCount + 1) / 2; // sum(1..maxCount)
 var startTime;
 
-function start() {
-  startTime = +new Date()
+function runner(func,name) {
+  function start() {
+    startTime = +new Date()
+  }
+  function stop(aSum) {
+    var elapsed = +new Date() - startTime;
+    log.info('time::%s %d ms for %d events', name, elapsed, maxCount);
+    log.info('%s::Expected %d, got %d: %s',name, expected, aSum, (expected === aSum) ? 'OK' : 'NOT OK');
+    return elapsed;
+  }
+  return function() {
+    return Promise.resolve(42)
+      .then(start)
+      .then(func)
+      .then(stop);
+  }
 }
 
-function stop() {
-  var elapsed =  +new Date() - startTime;
-  log.info('time: %d ms for %d events',elapsed,maxCount);
-  return elapsed;
-}
 
 function simple() {
   var sum = 0;
-  var expected = maxCount * (maxCount + 1) / 2; // sum(1..maxCount)
-
   for (var i = 1; i <= maxCount; i++) {
     sum += i;
   }
-  log.info('Expected %d, got %d: %s', expected, sum, (expected === sum) ? 'OK' : 'NOT OK');
+  return Promise.resolve(sum);
+}
 
+// with promises
+function simpleP() {
+  var i = 1;
+  var sum = 0;
+
+  function loop() {
+    // log.info('enter %d', i);
+    sum += i++;
+    var p = Promise.resolve(sum);
+    if (i <= maxCount) {
+      p = p.then(loop);
+    }
+    return p;
+  }
+
+  return loop();
 }
 
 Promise.resolve(42)
-  .then(start)
-  .then(simple)
-  .then(stop);
+  .then(runner(simple,'Simple'))
+  .then(runner(simpleP,'Promise'));
