18487e3b9b75d7c07eee6f0ba8f2865a23eb1fb4
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-07-31 20:49:14 -0400

    Move tasks to lib/

--- index.js
+++ index.js
@@ -1,66 +1,19 @@
 'use strict';
 
-var Prometheus = require('prometheus-client');
+// dependencies - core-public-internal
 var log = require('./lib/log');
 var metrics = require('./lib/metrics');
+var task = require('./lib/task');
 
 log.info('Queue.push(hello)');
 
+// start the web server (Prometheus /metrics for now)
 metrics.prometheus.listen(8000);
 // app = express()
 // app.get("/metrics",prometheus.metricsFunc())
 // app.listen(9090)
 
-// Setup iterating processes
-var maxCount = 1e6;
-var expected = maxCount * (maxCount + 1) / 2; // sum(1..maxCount)
-var startTime;
-
-function runner(func, name) {
-  function start() {
-    startTime = +new Date();
-  }
-
-  function stop(aSum) {
-    var elapsed = +new Date() - startTime;
-    log.info('time::%s %d ms for %d events', name, elapsed, maxCount);
-    log.info('%s::Expected %d, got %d: %s', name, expected, aSum, (expected === aSum) ? 'OK' : 'NOT OK');
-    return elapsed;
-  }
-  return function() {
-    return Promise.resolve(42)
-      .then(start)
-      .then(func)
-      .then(stop);
-  };
-}
-
-function simple() {
-  var sum = 0;
-  for (var i = 1; i <= maxCount; i++) {
-    sum += i;
-  }
-  return Promise.resolve(sum);
-}
-
-// with promises
-function simpleP() {
-  var i = 1;
-  var sum = 0;
-
-  function loop() {
-    // log.info('enter %d', i);
-    sum += i++;
-    var p = Promise.resolve(sum);
-    if (i <= maxCount) {
-      p = p.then(loop);
-    }
-    return p;
-  }
-
-  return loop();
-}
-
+// start some tasks
 Promise.resolve(42)
-  .then(runner(simple, 'Simple'))
-  .then(runner(simpleP, 'Promise'));
+  .then(task.runner(task.tasks.simple, 'Simple'))
+  .then(task.runner(task.tasks.simpleP, 'Promise'));
