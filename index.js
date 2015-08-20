681adaf287bb5851da29402a202331ced8cbf285
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-08-19 20:11:43 -0400

    Metrics and logging, start front end (react)

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 'use strict';
 
 // dependencies - core-public-internal
+var path = require('path');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var log = require('./lib/log');
 var metrics = require('./lib/metrics');
 var task = require('./lib/task');
 
 log.info('Queue.push(hello)\n');
 
-// start the web server (Prometheus /metrics for now)
-metrics.prometheus.listen(8000);
-// app = express()
-// app.get("/metrics",prometheus.metricsFunc())
-// app.listen(9090)
+// move to config
+var port = process.env.PORT || 8000;
+server.listen(port, function() {
+  log.info('Express server listening on port *:' + port);
+});
+
+app.use(morgan('dev', { // or tiny'
+  skip: function(req, res) {
+    return req.url === '/metrics';
+  }
+}));
+
+// Instrument for prometheus
+app.get('/metrics', metrics.prometheus.metricsFunc());
+
+// static app
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket) {
+  io.emit('info', {
+    msg: 'user connected'
+  });
+  socket.on('disconnect', function() {
+    io.emit('info', {
+      msg: 'user disconnected'
+    });
+  });
+});
+
+metrics.setSockio(io);
+
+setInterval(function() {
+  var opts = {
+    name: 'batch_rate',
+    engine: 'Fake',
+  };
+  // io.emit('gauge', opts);
+  metrics.set(opts,Math.floor(Math.random() * 1000));
+
+  var opts2 = {
+    name: 'push_total',
+    engine: 'Fake'
+  };
+  metrics.increment(opts2, 1000);
+}, 1000);
 
 // start some tasks
 function tryemall() {
---
+++ public/app.js
@@ -0,0 +1,81 @@
+/**
+ * @jsx React.DOM
+ */
+
+var socket = io.connect();
+
+
+var Message = React.createClass({
+  render: function(){
+    var cx = React.addons.classSet;
+    var classes = cx({
+      'odd': this.props.message.id%2,
+    });
+    return(
+      <li className={classes}>
+        {this.props.message.stamp} : {this.props.message.msg}
+      </li>
+    )
+  }
+});
+
+var MessageList = React.createClass({
+  render: function(){
+    var renderMessage = function(message){
+      return <Message key={message.id} message={message} />
+    }
+    return (
+      <div className="section">
+        <h2> Messages: </h2>
+        <ul className="messages">
+          { this.props.messages.map(renderMessage)}
+        </ul>
+      </div>
+    );
+  }
+});
+
+
+
+var uniqueId=999;
+var ChatApp = React.createClass({
+
+  getInitialState: function(){
+
+    socket.on('info', this.messageReceive);
+    socket.on('gauge', this.valueReceive);
+
+    return {messages:[]};
+  },
+
+  valueReceive: function(opts){
+    this.messageReceive({
+      msg: JSON.stringify(opts)
+    });
+  },
+  messageReceive: function(message){
+    console.log('message',message);
+    var maxMessages = 10;
+    var messages = this.state.messages;
+    message.id = uniqueId++;
+    message.stamp = new Date().toISOString();
+
+    messages.unshift(message);
+    while (messages.length>maxMessages){
+      messages.pop();
+    }
+    this.forceUpdate();
+  },
+
+  render : function(){
+    return (
+      <div>
+        <MessageList messages={this.state.messages} />
+      </div>
+    );
+  }
+});
+
+React.render(<ChatApp />, document.getElementById('content'));
+
+
